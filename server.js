0a30e2d4d31cb3790df3eac3d41298db45a1b1b7
rantmed <rantmed87@gmail.com>
2017-10-04 13:19:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var fs = require('fs');
 //var counties = JSON.parse(fs.readFileSync(path.join(__dirname, 'scripts', 'prueba_distrito.geojson'), {encoding: 'utf8'}));
 
 
-
-connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8000)
+var port    =   process.env.PORT || 8000;
+connect().use(serveStatic(__dirname)).listen('port')
 });
 // cd navega hasta la carpeta 
 // npm install connect serve-static
